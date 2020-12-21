c2b7adfd7841f6b3dbb97c6b7899918d8131118c
Olufayojimi Olawore <ooo30@cam.ac.uk>
2020-12-21 22:10:58 +0000

    Revert "tim"
    This reverts commit 0edc8048a91509a2c515b2fd2348a2ea5a0a91df.

--- src/boot/ksdk1.1.0/devHCSR04.c
+++ src/boot/ksdk1.1.0/devHCSR04.c
@@ -3,13 +3,14 @@
 
 #include "fsl_spi_master_driver.h"
 #include "fsl_port_hal.h"
-#include "fsl_lptmr_driver.h"
+//#include "fsl_lptmr_driver.h"
 #include "fsl_hwtimer.h"
 #include "fsl_debug_console.h"
 #include "fsl_clock_manager.h"
 
 #define HWTIMER_LL_DEVIF kSystickDevif
-#define HWTIMER_LL_ID 1
+#define SAMP kPitDevif
+#define HWTIMER_LL_ID 0
 
 #define HWTIMER_ISR_PRIOR 5
 #define HWTIMER_PERIOD 1000
@@ -76,7 +77,7 @@ takeReading()
 	
 	SEGGER_RTT_printf(0, "Starting now\n");
 
-	if (kHwtimerSuccess != HWTIMER_SYS_Init(&hwtimer, &HWTIMER_LL_DEVIF, HWTIMER_LL_ID, HWTIMER_ISR_PRIOR, NULL))
+	if (kHwtimerSuccess != HWTIMER_SYS_Init(&hwtimer, &SAMP, HWTIMER_LL_ID, 1, NULL))
     {
         SEGGER_RTT_printf(0,"\r\nError: hwtimer initialization.\r\n");
     }
