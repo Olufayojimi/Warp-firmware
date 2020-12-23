ef3d6e75d80afb27c31b0d9c513254fc2269529a
Olufayojimi Olawore <ooo30@cam.ac.uk>
2020-12-23 12:40:51 +0000

    yes

--- src/boot/ksdk1.1.0/devHCSR04.c
+++ src/boot/ksdk1.1.0/devHCSR04.c
@@ -200,9 +200,11 @@ takeReading()
 
     	//SEGGER_RTT_printf(0,"timer stopped\n");
     }
-    SEGGER_RTT_printf(0,"%d\n", total/10);
+    int avg = total/10;
+    avg = -1.24 * (avg-672);
+    SEGGER_RTT_printf(0,"%d\n", avg);
     
-    return total/10;
+    return avg;
 
 	
 	
